87173e8b4ee5f422013cbc8c9ac4c58e130bfc07
martin <imbedoya@tekmexico.com>
2021-06-09 11:18:50 -0700

    Tested indexeddb

--- 44.CustomHooks/custom-hooks/src/CustomHooks/useIndexedDB/useIndexedDB.js
+++ 44.CustomHooks/custom-hooks/src/CustomHooks/useIndexedDB/useIndexedDB.js
@@ -1,4 +1,4 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useMemo, useReducer, useRef } from "react";
 
 function reducer(state, action) {
   switch (action.type) {
@@ -11,6 +11,9 @@ function reducer(state, action) {
     case "CLEAR": {
       return [];
     }
+    case "SET": {
+      return [...action.payload];
+    }
     default:
       return state;
   }
@@ -21,75 +24,105 @@ function action(type, payload) {
 }
 
 export default function useIndexedDB(store = "default", dbName = "myDB") {
-  const [state, dispatch] = useReducer(reducer, []);
+  const dateVersion = useMemo(() => Date.now(), []);
+  // const request = indexedDB.open(dbName, dateVersion);
 
-  const dateVersion = useMemo(()=>Date.now(), []);
-  const request = indexedDB.open(dbName, dateVersion);
-  let db = new Promise((res) => {
-    request.onsuccess = function (event) {
-      const _db = event.target.result;
-      res(_db);
+  let db = useRef({});
+
+  function initDB() {
+    const request = window.indexedDB.open(dbName, dateVersion);
+    request.onerror = function (event) {
+      console.error(event.target);
     };
-  });
-
-  request.onupgradeneeded = function (event) {
-    const db = event.target.result;
-    try {
-      db.createObjectStore(store, { autoIncrement: true });
-    } catch (e) {}
-    console.log("done upgrade");
-  };
-
-  function getAsyncStore(){
-    return db.then((_db) =>
-      _db.transaction(store, "readwrite").objectStore(store)
-    );
-  };
-
-  function getStoredData(){
-    const objectStore = getAsyncStore();
-    objectStore.then((store) => {
-      dispatch(action("CLEAR"));
-      const cursorTransaction = store.openCursor();
-      cursorTransaction.onsuccess = function (event) {
-        const cursor = event.target.result;
-        if (cursor) {
-          const record = { key: cursor.key, value: cursor.value };
-          console.log({ key: cursor.key, value: cursor.value });
-          dispatch(action("ADD", record));
-          cursor.continue();
-        }
+    request.onupgradeneeded = function (event) {
+      const db = event.target.result;
+      try {
+        db.createObjectStore(store, { autoIncrement: true });
+      } catch (e) {
+        db.deleteObjectStore(store);
+        db.createObjectStore(store, { autoIncrement: true });
+      }
+    };
+    return new Promise((res) => {
+      request.onsuccess = function (event) {
+        const _db = event.target.result;
+        // getStoredData(_db);
+        res(_db);
       };
-    });
-  };
-
-  useEffect(() => {
-    getStoredData();
-  }, []);
-
-  function remove(key) {
-    const objStore = getAsyncStore();
-    objStore.then((store) => {
-      const removeOperation = store.delete(key);
-      removeOperation.onsuccess = function (event) {
-        getStoredData();
-        };
     });
   }
 
-  function update(item, key){
-    const objStore = getAsyncStore();
-    objStore.then((store)=>{
-      const updateOperation = store.put(item, key);
-      updateOperation.onsuccess = function(e){
-        getStoredData();
+  db.current = useMemo(() => initDB(), []);
+
+  function getAsyncStore(db) {
+    return db
+      .then((_db) => _db.transaction(store, "readwrite"))
+      .then((transaction) => transaction.objectStore(store));
+  }
+
+  async function getStoredData(db) {
+    const data = await getIndexedData(db);
+    dispatch(action("SET", data ));
+  }
+
+  // async function setStoredData() {
+  //   const data = await getIndexedData(db);
+  //   return data;
+  // }
+
+  function getIndexedData(db) {
+    return new Promise((res) => {
+      const objectStore = getAsyncStore(db);
+      const acc = [];
+      objectStore.then((store) => {
+        const cursorTransaction = store.openCursor();
+        cursorTransaction.onsuccess = function (event) {
+          const cursor = event.target.result;
+          if (cursor) {
+            const record = { key: cursor.key, value: cursor.value };
+            acc.push(record);
+            cursor.continue();
+          } else {
+            res(acc);
+          }
+        };
+      });
+    });
+  }
+
+  useEffect(() => {
+    getStoredData(db.current);
+    return async function clean(){
+      const _db = await db.current;
+      _db.close();
+    };
+  }, []);
+
+  function remove(key) {
+    const objStore = getAsyncStore(db.current);
+    objStore.then((store) => {
+      const removeOperation = store.delete(key);
+      removeOperation.onsuccess = function (event) {
+        getStoredData(db.current);
       };
     });
-  };
+  }
+
+  function update(item, key) {
+    const objStore = getAsyncStore(db.current);
+    objStore.then((store) => {
+      const updateOperation = store.put(item, key);
+      updateOperation.onsuccess = function (e) {
+        getStoredData(db.current);
+      };
+    });
+  }
+
+  const [state, dispatch] = useReducer(reducer, []);
 
   return {
     put: update,
     store: state,
-    remove
+    remove,
   };
 }
--- 44.CustomHooks/custom-hooks/src/CustomHooks/useIndexedDB/useIndexedDB.test.js
+++ 44.CustomHooks/custom-hooks/src/CustomHooks/useIndexedDB/useIndexedDB.test.js
@@ -1,45 +1,12 @@
 import useIndexedDB from "./useIndexedDB";
-import { cleanup, render } from "@testing-library/react";
-import { useEffect } from "react";
-
-function fakeIndexedDB() {
-  const openRequest = {
-    onsuccess: null,
-    callOnSuccess() {
-      if (typeof this.onsuccess === "function") {
-        const event = { target: { objectStore, transaction } };
-        this.onsuccess(event);
-      }
-    },
-    onupgradeneeded: null,
-    callOnUpgradeneeded() {
-      if (typeof this.onupgradeneeded === "function") {
-        const event = { target: { result: { objectStore, transaction } } };
-      }
-    },
-  };
-
-  const objectStore = {
-    store: null,
-    getAll() {},
-  };
-
-  const transaction = {};
-
-  window.indexedDB = {
-    open(name, version) {
-      setTimeout(() => {
-        openRequest.callOnSuccess();
-        openRequest.callOnUpgradeneeded();
-      }, 100);
-      return openRequest;
-    },
-  };
-}
+import { cleanup, render, waitFor, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import { useEffect, useState } from "react";
+import "fake-indexeddb/auto";
+import fireEvent from "@testing-library/user-event";
 
 describe("Testing hook for indexed db", () => {
   beforeEach(() => {
-    fakeIndexedDB();
+    jest.useFakeTimers();
   });
 
   afterEach(cleanup);
@@ -62,26 +29,81 @@ describe("Testing hook for indexed db", () => {
         expect(store).toHaveLength(0);
         expect(put).toEqual(expect.any(Function));
         expect(remove).toEqual(expect.any(Function));
-      });
+      }, []);
       return <></>;
     }
     render(<Test />);
   });
 
-  test("Adding data to indexedDB", () => {
+  test("Adding data to indexedDB", async () => {
     function Test() {
       const { store, put, remove } = useIndexedDB();
-
       useEffect(() => {
         put("Testing");
-      })
-      useEffect(() => {
-        expect(store).toHaveLength(1);
-        expect(put).toEqual(expect.any(Function));
-        expect(remove).toEqual(expect.any(Function));
-      });
-      return <></>;
+      }, []);
+      return (
+        <>
+          {store.map((v, index) => (
+            <p key={index}>{v.value}</p>
+          ))}
+        </>
+      );
     }
     render(<Test />);
+    const displayed = await screen.findByText("Testing");
+    expect(displayed).toBeDefined();
+  });
+
+  test("Deleting data from indexedDB", async () => {
+    function Test() {
+      const [name, setName] = useState("");
+
+      const { store, put, remove } = useIndexedDB();
+
+      function addEvent(e) {
+        e.preventDefault();
+        put(name);
+        setName("");
+      }
+
+      return (
+        <div>
+          <label>
+            name
+            <input value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+          {store.map((v, index) => (
+            <p
+              key={index}
+              onClick={(e) => {
+                remove(v.key);
+              }}
+            >
+              {v.value}
+            </p>
+          ))}
+          <button onClick={addEvent}>Add</button>
+          <button>Remove</button>
+        </div>
+      );
+    }
+    render(<Test />);
+    const hello = "hello",
+      world = "world";
+    const nameInput = screen.getByLabelText("name");
+    fireEvent.type(nameInput, hello);
+    const addButton = screen.getByRole("button", { name: "Add" });
+    fireEvent.click(addButton);
+    const helloNote = await screen.findByText(hello);
+    expect(helloNote).toBeDefined();
+    fireEvent.type(nameInput, world);
+    fireEvent.click(addButton);
+    const worldNote = await screen.findByText(world);
+    expect(worldNote).toBeDefined();
+    fireEvent.click(helloNote);
+    // waitForElementToBeRemoved(helloNote).then(()=>expect(helloNote).not.toBeDefined())
+    await waitFor(()=>{
+      expect(screen.queryByText(hello)).not.toBeInTheDocument();
+    });
   });
 });
